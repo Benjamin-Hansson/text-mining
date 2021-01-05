5c603c266531810d7bac04f00c5e139fad0ddf82
Benjamin Hansson <benha109@student.liu.se>
2021-01-05 12:59:51 +0100

    switching computer

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import praw
 import requests
 import json
+import pandas
 from pymongo import MongoClient, ASCENDING
 import time
+from alpha_vantage.timeseries import TimeSeries
+
 
 subreddits = ["stocks", "investing"]
 
@@ -28,6 +31,14 @@ def download_wsb_daily_discussion(start_timestamp, end_timestamp, increment):
         json_response = json.loads(response.text)
         print(f"Done downloading, found {len(json_response['data'])} posts")
         for post in json_response["data"]:
+            mongo_posts.delete_many({"id": post["id"]})
+
+        if json_response["data"]:
+            mongo_posts.insert_many(json_response["data"])
+
+
+        """
+        for post in json_response["data"]:
             print("-"*20)
             print(f"processing post: {post['full_link']}")
             print("Downloading comments...")
@@ -45,7 +56,7 @@ def download_wsb_daily_discussion(start_timestamp, end_timestamp, increment):
                 comment["post_id"] = post_id
             if comments["data"]:
                 mongo_comments.insert_many(comments["data"])
-
+"""
 
 def praw_download():
     reddit = praw.Reddit(
@@ -81,16 +92,64 @@ def getSubComments(comment, allComments, verbose=True):
 
 
 
+def download_alpha_vantage():
+    ts = TimeSeries(key="M8RF5RFRF8PG32I1", output_format='pandas')
+    res = ts.get_daily_adjusted(symbol="SPY", outputsize="full")[0]
+
+    res = res.drop(["1. open", "2. high", "3. low", "4. close", "6. volume", "7. dividend amount", "8. split coefficient"], axis=1)
+    res = res.rename(columns={'5. adjusted close': "close"})
+    print(res.columns)
+    print(res)
+
 
 def main():
+    from alpha_vantage.timeseries import TimeSeries
+    import pandas
+    ts = TimeSeries(key="M8RF5RFRF8PG32I1", output_format='pandas')
+    res = ts.get_daily_adjusted(symbol="SPY", outputsize="full")[0]
+    res = res.drop(
+        ["1. open", "2. high", "3. low", "4. close", "6. volume", "7. dividend amount", "8. split coefficient"], axis=1)
+    res = res.rename(columns={'5. adjusted close': "close"})
+    from datetime import datetime, timedelta
 
-    start_date = 1546300800 # 2019/01/01
-    start_date = 1596844800
+    one_day_offset = timedelta(days=1)
+    post_ids = mongo_posts.distinct('id')
+    for post_id in post_ids:
+        comments = mongo_comments.find({"post_id": post_id})
+        countr = 0
 
-    end_date = int(time.time())
-    #praw_download()
-    download_wsb_daily_discussion(start_date, end_date, WEEK_IN_SECONDS)
+        for comment in comments:
+            countr += 1
+            start_day = datetime.fromtimestamp(comment["created_utc"]).date()
 
+            date = datetime.fromtimestamp(comment["created_utc"]).date() + one_day_offset
+            while res.loc[res.index == str(date)].empty:
+                date = date + one_day_offset
+            next_trading_day = date
+
+            date = start_day
+            while res.loc[res.index == str(date)].empty:
+                date = date - one_day_offset
+            previous_trading_day = date
+            next_close = float(res.loc[res.index == str(next_trading_day)]["close"])
+            prev_close = float(res.loc[res.index == str(previous_trading_day)]["close"])
+
+            if next_close >= prev_close:
+                positive_day = True
+            else:
+                positive_day = False
+            if countr % 1000 == 0:
+                print(f"comment day: {start_day}, next trading day: {next_trading_day}, positive_day: {positive_day}")
+
+            mongo_comments.update_one({"id": comment["id"], "post_id": post_id},
+                                      {"$set": {"next_trading_day": str(next_trading_day)}})
+
+            """mongo_comments.update_one({"id": comment["id"], "post_id": post_id},
+                                      {"$set": {"spy_closing_price": next_close}})
+
+            mongo_comments.update_one({"id": comment["id"], "post_id": post_id},
+                                      {"$set": {"next_trading_day_positive": positive_day}})
+            """
 
 if __name__ == '__main__':
     main()
